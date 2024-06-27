cc36b3f886bc86a669f2601981305562b3c26f77
Katerina S <e.kosheva@gmail.com>
2024-06-27 07:18:14 +0200

    Etape 4

--- Backend/script.js
+++ Backend/script.js
@@ -6,10 +6,42 @@
     const menuContainer = document.getElementById('menu-category');
     const modal = document.getElementById('myModal');
     const modalGallery = document.getElementById('modal-gallery');
+    const modalGalleryView = document.getElementById('modal-gallery-view');
+    const modalAddPhotoView = document.getElementById('modal-add-photo-view');
     const changeGallery = document.getElementById('lien-modification');
     const btnCloseModal = document.getElementsByClassName('close')[0];
+    const openAddPhotoBtn = document.getElementById('open-add-photo-btn');
+    const backToGalleryIcon = document.getElementById('back-to-gallery-icon');
+    const photoUploadForm = document.getElementById('photo-upload-form');
+    const photoCategorySelect = document.getElementById('photo-category');
+    const fileInput = document.getElementById('imageUrl');
+    const zonePreviewImage = document.getElementById('zone-ajout-image')
+    //const NoImage = document.getElementById('no-image')
+    //const OuiImage = document.getElementById('oui-image')
+    const ajoutFileLabel = document.querySelector('.custom-file-upload');
 
-    //***********FONCTIONS**************
+    //кастомизация выбора файла изображения
+    fileInput.addEventListener('change', (event) => {
+        const file = event.target.files[0];
+        const fileName = file ? file.name : 'Ajouter Photo';
+        ajoutFileLabel.textContent = fileName;
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                zonePreviewImage.innerHTML = `<img src="${e.target.result}" alt="Work Preview">`;
+                // NoImage.style.display = 'none';
+                // OuiImage.style.display = 'block'
+                // const workPreviewImg = document.getElementById('workPreview');
+                // workPreviewImg.src = e.target.result;
+                // workPreviewImg.alt = fileName; // Обновление атрибута alt
+                };
+            reader.readAsDataURL(file);
+        } else {
+            zonePreviewImage.innerHTML = '';
+        }
+    });
+//***********FONCTIONS**************
 
     // Fonction pour créer les boutons du menu
     function createMenuButtons(categories) {
@@ -30,6 +62,21 @@
             button.id = `menu-item-${category.id}`;
             button.addEventListener('click', () => filterWorksByCategory(category.id));
             menuContainer.appendChild(button);
+        });
+
+        // Remplir le sélecteur de catégorie dans le formulaire d'ajout de photo
+        populateCategorySelect(categories);
+}
+
+    // Fonction pour remplir le sélecteur de catégorie
+    function populateCategorySelect(categories) {
+        if (!photoCategorySelect) return;
+        photoCategorySelect.innerHTML = '';
+        categories.forEach(category => {
+            const option = document.createElement('option');
+            option.value = category.id;
+            option.text = category.name;
+            photoCategorySelect.appendChild(option);
         });
     }
 
@@ -56,6 +103,9 @@
         // On crée des éléments pour chaque œuvre
         works.forEach(work => {
             let nouvelleFigure = document.createElement("figure");
+            nouvelleFigure.classList.add('gallery-item');
+            nouvelleFigure.dataset.id = work.id; // Ajouter l'ID comme attribut de données
+
             let nouveauImg = document.createElement("img");
             let nouveauTitre = document.createElement("figcaption");
 
@@ -158,7 +208,7 @@
 
                 // Ajouter l'icône du panier
                 cartIcon.className = 'fa-solid fa-trash-can cart-icon';
-                cartIcon.dataset.id = work.id; // Привязываем id к иконке
+                cartIcon.dataset.id = work.id; // Ajouter l'ID comme attribut de données
 
                 galleryItem.appendChild(image);
                 galleryItem.appendChild(cartIcon);
@@ -171,7 +221,13 @@
                 icon.addEventListener('click', async (event) => {
                     const workId = event.target.dataset.id;
                     await deleteWork(workId);
-                    event.target.parentElement.remove(); // Удаление элемента из модального окна
+                    
+                    // Suppression element parent de gallerie sans recharger les pages
+                    event.target.parentElement.remove(); 
+                    const mainGalleryItem = document.querySelector(`.gallery-item[data-id="${workId}"]`);
+                    if (mainGalleryItem) {
+                        mainGalleryItem.remove();
+                    }                
                 });
 
             });
@@ -180,6 +236,7 @@
         }
     }
 
+    //fonction de suppresion des ouvres/works
     async function deleteWork(workId) {
         const authToken = localStorage.getItem('authToken'); // token de localStorage
         try {
@@ -198,6 +255,72 @@
         }
     }
 
+    // Ouvrir la vue Ajout Photo
+    openAddPhotoBtn.addEventListener('click', () => {
+        modalGalleryView.style.display = 'none';
+        modalAddPhotoView.style.display = 'block';
+    });
+
+    // Retourner à la vue Galerie Photo
+    backToGalleryIcon.addEventListener('click', () => {
+        modalAddPhotoView.style.display = 'none';
+        modalGalleryView.style.display = 'block';
+        // fileInput.value = '';
+
+    });
+
+    // Gestion du formulaire d'upload de photo
+    photoUploadForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        const authToken = localStorage.getItem('authToken');
+        // const formData = new FormData(photoUploadForm);
+    
+        const titleInput = document.getElementById('title');
+        const categoryInput = document.getElementById('photo-category');
+        const formData = new FormData();
+
+        formData.append('title', titleInput.value);
+        formData.append('category', parseInt(categoryInput.value)); // Conversion du code de la catégorie en entier
+ 
+        formData.append('image',fileInput.files[0]);
+
+        // const userId = localStorage.getItem('userId');
+        // if (userId) {
+        //     formData.append('userId', userId);
+        // }
+        
+        // Вывод содержимого FormData в консоль
+        for (let [key, value] of formData.entries()) {
+            console.log(`${key}: ${value}`);
+        }
+
+            try {
+            const response = await fetch('http://localhost:5678/api/works', {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${authToken}`
+                },
+                body: formData
+            });
+            if (!response.ok) {
+                throw new Error('Erreur lors de l\'upload de la photo ' + response.statusText);
+            }
+
+            // Очистка поля выбора файла после успешной загрузки
+            fileInput.value = '';
+            // Après l'upload réussi, retourner à la vue Galerie Photo
+            modalAddPhotoView.style.display = 'none';
+            modalGalleryView.style.display = 'block';
+            // Réinitialiser le formulaire et revenir à la vue de la galerie
+            photoUploadForm.reset();
+            //**************заменить 
+            chargerGalerieModal(); // Rafraîchir la galerie
+            //**************** */
+        } catch (error) {
+            console.error('Erreur lors de l\'upload de la photo :', error);
+        }
+    });
+
     changeGallery.addEventListener('click', () => {
         modal.style.display = "block";
         chargerGalerieModal()
@@ -206,15 +329,34 @@
     // Lorsque l'utilisateur clique sur <span> (x), fermez le modal
     btnCloseModal.onclick = function () {
         modal.style.display = "none";
+        modalAddPhotoView.style.display = 'none';
+        modalGalleryView.style.display = 'block';
         filterWorksByCategory("*")
     }
 
     // Lorsque l'utilisateur clique n'importe où en dehors du modal, fermez-le
     window.onclick = function (event) {
         if (event.target === modal) {
+            modalAddPhotoView.style.display = 'none';
+            modalGalleryView.style.display = 'block';
             modal.style.display = "none";
             filterWorksByCategory("*")
         }
     }
 
+            // fileInput.addEventListener('change', (event) => {
+            //     const file = event.target.files[0];
+            //     const fileName = file ? file.name : 'Ajouter Photo';
+            //     customLabel.textContent = fileName;
+
+            //     if (file) {
+            //         const reader = new FileReader();
+            //         reader.onload = function(e) {
+            //             preview.innerHTML = `<img src="${e.target.result}" alt="Image Preview">`;
+            //         };
+            //         reader.readAsDataURL(file);
+            //     } else {
+            //         preview.innerHTML = '';
+            //     }
+            // });
 //});
